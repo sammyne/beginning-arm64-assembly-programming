2c4181931733d30577121252b416d41152415b8a
sammyne <xiangminli@alumni.sjtu.edu.cn>
2023-02-27 05:36:39 +0000

    doc: add ref link for 'Procedure Call Standard'

--- chapter01/hello.s
+++ chapter01/hello.s
@@ -9,11 +9,13 @@ msg: .asciz "hello, world"
 
   .text
 main: 
-  mov x0, #1    // 1 = to stdout
-  adr x1, msg   // string to display in rsi,
-  mov x2, #12  // length of the string, without 0
+              // Procedure Call Standard sees
+              //  https://developer.arm.com/documentation/102374/0101/Procedure-Call-Standard
+  mov x0, #1  // 1 = to stdout
+  adr x1, msg // string to display in rsi,
+  mov x2, #12 // length of the string, without 0
   mov x8, #64 // syscall #64 is the write() function
-  svc #0       // display the string
+  svc #0      // display the string
   mov x0, 0   // 0 = success exit code 
-  mov x8, #93    // 60 = exit
-  svc #0       // quit
+  mov x8, #93 // 60 = exit
+  svc #0      // quit
